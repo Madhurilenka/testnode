c24694ca85dadaaf51acc9733c2486281f23c25f
unknown <madhurilenka98@gmail.com>
2023-02-22 14:31:15 +0530

    update

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const route = require('./route/route.js');
+ 
+const app = express( )
+
+
+app.use(bodyParser.json());
+
+   
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
